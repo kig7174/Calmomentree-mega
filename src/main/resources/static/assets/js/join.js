c59ac4df511e13ac098052da02fc8e5dc88ca7d6
ingyeom <kig7174@naver.com>
2024-10-31 17:52:17 +0900

    Design: 24/10/31 회원 > 회원가입 약관동의 체크박스
    전체 약관 동의 시 전체 항목 체크하기, 항목 중 체크 해제 시 전체 약관
    동의 체크 해제 (50%)
    
    Related to #20

--- src/main/resources/static/assets/js/join.js
+++ src/main/resources/static/assets/js/join.js
@@ -22,3 +22,29 @@ document.querySelector("#password").addEventListener("blur", e => {
     document.querySelector("#password-focus").classList.remove("password-on");
     document.querySelector("#password-focus").classList.add("password-off");
 });
+
+/** 전체 약관 동의 */
+// 전체 동의 체크 시 모든 항목 체크
+document.querySelectorAll("input[data-checked]").forEach((v, i) => {
+    v.addEventListener('change', e => {
+        const current = e.currentTarget;
+        const targetSelector = current.dataset.checked;
+
+        document.querySelectorAll(targetSelector).forEach((v2, i2) => {
+            v2.checked = current.checked;
+        });
+    });
+});
+
+// 항목 중 체크가 풀리는 항목이 있다면 전체 동의 체크 해제
+const checkAll = document.querySelectorAll(".check-all");
+
+checkAll.forEach((v, i) => {
+    v.addEventListener("change", e => {
+        if (!v.checked) {
+            document.querySelector("#agree-all-checked").checked = false;
+        }
+    });
+});
+
+/** 약관 항목 버튼 클릭 시 상세 내용 열기 */
