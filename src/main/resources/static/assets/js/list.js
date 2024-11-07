0466bc1b4485aa3b78e122fcfb3966c2ba21822d
ingyeom <kig7174@naver.com>
2024-11-07 17:36:03 +0900

    Design: 24/11/07 상품 > 목록 HTML, CSS, Javascript 추가
    기존 프로젝트에서 코드 복사 / 카테고리 수정 필요 / 마우스 오버 시 이미지
    변경 기능 추가 예정
    
    Related to #11

---
+++ src/main/resources/static/assets/js/list.js
@@ -0,0 +1,23 @@
+/**
+ * product list js
+ */
+
+
+// image mouse over
+const mouseover = document.querySelectorAll('.list-item-img');
+const visible = document.querySelectorAll('.list-img-visibility');
+const hidden = document.querySelectorAll('.list-img-hidden');
+
+for (let i = 0; i < mouseover.length; i++) {
+    if (hidden[i].classList.contains("list-img-hidden")) {
+        mouseover[i].addEventListener('mouseover', e => {
+            visible[i].style.display = "none";
+            hidden[i].style.display = "block"
+        });
+
+        mouseover[i].addEventListener('mouseout', e => {
+            visible[i].style.display = "block";
+            hidden[i].style.display = "none"
+        });
+    }
+}
