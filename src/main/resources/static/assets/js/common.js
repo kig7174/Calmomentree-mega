6860e8bc71785c70495e7bbb92bb3ccc46e76e04
ingaeng2 <kig7174@naver.com>
2024-11-09 00:44:50 +0900

    Design: 24/11/09 HTML thymeleaf Test join page
    html head 태그 추가 / script 추가 / thymeleaf 적용에 따라 CSS 수정

--- src/main/resources/static/assets/js/common.js
+++ src/main/resources/static/assets/js/common.js
@@ -1,3 +1,4 @@
+// 메뉴 및 배너 스크롤 이벤트
 window.addEventListener("scroll", (e) => {
     const scrollTop = window.scrollY;
 
@@ -9,6 +10,18 @@ window.addEventListener("scroll", (e) => {
         document.querySelector(".menu").classList.remove("on");
     }
 });
+
+// 브라우저 창 중간에서 새로고침 시 메뉴가 브라우저 상단 끝으로 이동하지 않음
+// 아래 코드 추가
+const scrollTop = window.scrollY;
+
+if(scrollTop > 0){
+    document.querySelector(".banner").classList.add("on");
+    document.querySelector(".menu").classList.add("on");
+} else {
+    document.querySelector(".banner").classList.remove("on");
+    document.querySelector(".menu").classList.remove("on");
+}
 
 document.querySelector(".category-inner").addEventListener("mouseover", (e) => {
     e.preventDefault();
