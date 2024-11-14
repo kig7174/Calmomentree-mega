12060dc413bc3a58d477f71ef9bc75fbda3eb215
bongja <jiuk0209@gmail.com>
2024-11-14 18:02:21 +0900

    Chore : 24/11/14 코드수정사항

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -1,8 +1,3 @@
-// index page margin remove
-(function() {
-    document.querySelector(".container").style.margin = "0";
-})();
-
 // best seller swiper
 let bestSeller = new Swiper(".best-seller", {
     slidesPerView: "auto",
