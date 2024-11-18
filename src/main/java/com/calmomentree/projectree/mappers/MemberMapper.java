6c3cbf7bffe71278d86a140227bc26da36106e57
ingaeng2 <kig7174@naver.com>
2024-11-19 01:36:04 +0900

    Feat: 24/11/19 회원 SQL 추가
    update, delete, selectItem, selectList, selectCount 추가 // update문
    수정 필요 (비밀번호 변경)
    
    Related to #44

--- src/main/java/com/calmomentree/projectree/mappers/MemberMapper.java
+++ src/main/java/com/calmomentree/projectree/mappers/MemberMapper.java
@@ -38,25 +38,79 @@ public interface MemberMapper {
     @Options(useGeneratedKeys = true, keyProperty = "memberId", keyColumn = "member_id")
     public int insert(Member input);
 
-    @Update("...")
+    @Update("<script> " +
+            "UPDATE members SET " + 
+            // 입력된 비밀번호가 현재 비밀번호와 다를 때 비밀번호 변경
+            // ? 새 비밀번호 입력없이 비밀번호 변경하려면 WHERE절에서 비밀번호가 같은 지 확인하면 안됨
+            // ? WHERE절에서 비밀번호가 같은 지 확인을 안해도 되나?
+            "user_pw = " +
+            "( " +
+            "CASE " +
+            "WHEN user_pw=MD5(#{userPw}) " +
+            "THEN user_pw " +
+            "WHEN user_pw!=MD5(#{userPw}) " +
+            "THEN #{userPw} " +
+            "END " +
+            "), " +
+            // Dynamic SQL if문에서 파라미터값 받는 방법?
+            // "<if test='userPw != null AND userPw != \"\" AND userPw != #{userPw}'>user_pw=#{userPw},</if> " +
+            "tel=#{tel}, " +
+            "email=#{email}, " +
+            "postcode=#{postcode}, " +
+            "addr1=#{addr1}, " +
+            "addr2=#{addr2}, " +
+            "<if test='birthday != null and birthday != \"\"'>birthday=#{birthday},</if> " +
+            "edit_date=NOW() " +
+            "WHERE member_id=#{memberId} AND user_pw=MD5(#{userPw}) " +
+            "</script>")
     public int update(Member input);
 
-    @Delete("...")
+    @Delete("DELETE FROM members WHERE member_id=#{memberId}")
     public int delete(Member input);
 
-    @Select("...")
-    @Results(id="resultMap", value={
-        @Result(property="...", column="..."),
-        @Result(property="...", column="..."),
-        @Result(property="...", column="...")
+    @Select("SELECT member_id, user_name, user_id, user_pw, " +
+            "tel, email, postcode, addr1, addr2, " +
+            "birthday, is_marketing_agree, " +
+            "login_date, join_date, edit_date, " +
+            "is_out, is_admin " +
+            "FROM members " +
+            "WHERE member_id=#{memberId}")
+    @Results(id = "MemberMap", value = {
+        @Result(property="memberId", column="member_id"),
+        @Result(property="userName", column="user_name"),
+        @Result(property="userId", column="user_id"),
+        @Result(property="userPw", column="user_pw"),
+        @Result(property="tel", column="tel"),
+        @Result(property="email", column="email"),
+        @Result(property="postcode", column="postcode"),
+        @Result(property="addr1", column="addr1"),
+        @Result(property="addr2", column="addr2"),
+        @Result(property="birthday", column="birthday"),
+        @Result(property="isMarketingAgree", column="is_marketing_agree"),
+        @Result(property="loginDate", column="login_date"),
+        @Result(property="joinDate", column="join_date"),
+        @Result(property="editDate", column="edit_date"),
+        @Result(property="isOut", column="is_out"),
+        @Result(property="isAdmin", column="is_admin")
     })
     public Member selectItem(Member input);
 
-    @Select("...")
-    @ResultMap("resultMap")
+    @Select("SELECT member_id, user_name, user_id, user_pw, " +
+            "tel, email, postcode, addr1, addr2, " +
+            "birthday, is_marketing_agree, " +
+            "login_date, join_date, edit_date, " +
+            "is_out, is_admin " +
+            "FROM members")
+    @ResultMap("MemberMap")
     public List<Member> selectList(Member input);
 
-    @Select("...")
+    @Select("<script> " +
+            "SELECT COUNT(*) FROM members " +
+            "<where> " +
+            "<if test='userId != null'>user_id = #{userId}</if> " +
+            "<if test='memberId != 0'>AND member_id != #{memberId}</if>" +
+            "</where> " +
+            "</script>")
     public int selectCount(Member input);
 }
 
