5487b436659110cac7835ba722b6edce1170a561
ingaeng2 <kig7174@naver.com>
2024-11-18 23:46:05 +0900

    Style: 24/11/18 Member Model 변경
    카멜 표기법으로 변경
    
    Related to #42

--- src/main/java/com/calmomentree/projectree/models/Member.java
+++ src/main/java/com/calmomentree/projectree/models/Member.java
@@ -6,22 +6,22 @@ import lombok.Setter;
 
 @Data
 public class Member {
-    private int member_id;              // 회원번호
-    private String user_name;           // 회원 이름
-    private String user_id;             // 회원 아이디
-    private String user_pw;             // 회원 비밀번호
+    private int memberId;              // 회원번호
+    private String userName;           // 회원 이름
+    private String userId;             // 회원 아이디
+    private String userPw;             // 회원 비밀번호
     private String tel;                 // 전화번호
     private String email;               // 이메일
     private String postcode;            // 우편번호
     private String addr1;               // 기본주소
     private String addr2;               // 상세주소
     private String birthday;            // 생년월일
-    private String is_marketing_agree;  // 마케팅 수신 여부
-    private String login_date;          // 마지막 로그인 날짜
-    private String join_date;           // 회원가입 날짜
-    private String edit_date;           // 마지막 수정 날짜
-    private String is_out;              // 탈퇴여부
-    private String is_admin;            // 관리자 여부
+    private String isMarketingAgree;  // 마케팅 수신 여부
+    private String loginDate;          // 마지막 로그인 날짜
+    private String joinDate;           // 회원가입 날짜
+    private String editDate;           // 마지막 수정 날짜
+    private String isOut;              // 탈퇴여부
+    private String isAdmin;            // 관리자 여부
 
     @Getter
     @Setter
