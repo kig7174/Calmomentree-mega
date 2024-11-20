558c8a25a76e181d84ca9df42ea96205227cb8a1
garibong <jiuk0209@gmail.com>
2024-11-21 01:02:03 +0900

    Rename : 24/11/21 BoardMapperTest.java 파일명 오타수정.

--- src/test/java/com/calmomentree/projectree/mappers/BoardMapperTest.java
+++ src/test/java/com/calmomentree/projectree/mappers/BoardMapperTest.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
-public class BoardMepperTest {
+public class BoardMapperTest {
     
     @Autowired
     private BoardMapper boardMapper;
