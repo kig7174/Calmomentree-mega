680dfd3beac74edf31da9d6196dad9df4eae1999
ingaeng2 <kig7174@naver.com>
2024-11-21 01:59:23 +0900

    Feat: 24/11/21 회원 > 회원가입 통합구현
    회원가입처리 유효성 검사 / 아이디 중복 검사 / 마케팅 수신 동의 추가
    
    Related to #47

--- src/main/java/com/calmomentree/projectree/controllers/apis/MemberRestController.java
+++ src/main/java/com/calmomentree/projectree/controllers/apis/MemberRestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
 public class MemberRestController {
     @Autowired
@@ -27,6 +26,12 @@ public class MemberRestController {
     @Autowired
     private MemberService memberService;
 
+    /**
+     * 아이디 중복 검사
+     * 
+     * @param userId - 입력한 아이디
+     * @return       - 중복되지 않으면 메세지 리턴
+     */
     @GetMapping("/api/member/id_unique_check")
     public Map<String, Object> idUniqueCheck(@RequestParam ("user_id") String userId) {
         try {
@@ -38,11 +43,17 @@ public class MemberRestController {
         return restHelper.sendJson(200, "는 사용 가능한 아이디입니다.", null, null);
     }
     
+    /**
+     * 회원 가입 처리
+     * 
+     * @return
+     */
     @PostMapping("/api/member/join")
     public Map<String, Object> join(  
         @RequestParam("user_id") String userId,
         @RequestParam("user_pw") String userPw,
         @RequestParam("user_name") String userName,
+        @RequestParam("pw_confimr") String pwConfirm,
         @RequestParam("tel1") String tel1,
         @RequestParam("tel2") String tel2,
         @RequestParam("tel3") String tel3,
@@ -52,17 +63,47 @@ public class MemberRestController {
         @RequestParam("addr2") String addr2,
         @RequestParam(value = "birthday", required = false) String birthday,
         @RequestParam("is_sms_agree") String isSmsAgree,
-        @RequestParam("is_email_agree") String isEmailAgree
+        @RequestParam("is_email_agree") String isEmailAgree,
+        @RequestParam("agree_service_check") String agreeServiceCheck,
+        @RequestParam("agree_privacy_check") String agreePrivacyCheck
     ) {
+        /** 전화번호 결합 */
         String tel = tel1 + tel2 + tel3;
 
+        /** 유효성 검사 */
         try {
             regexHelper.isValue(userId, "아이디를 입력해주세요.");
+            regexHelper.isLowerEngNum(userId, "아이디는 영문소문자와 숫자로 입력해주세요.");
+
+            regexHelper.isValue(userPw, "비밀번호를 입력해주세요.");
+            regexHelper.isPassword(userPw, "비밀번호 형식이 잘못되었습니다.");
+            regexHelper.isMatch(userPw, pwConfirm, "비밀번호확인이 일치하지 않습니다.");
             
+            regexHelper.isValue(userName, "이름을 입력해주세요.");
+            regexHelper.isKor(userName, "이름은 한글로만 입력해주세요.");
+
+            regexHelper.isValue(tel, "전화번호를 입력해주세요.");
+            regexHelper.isPhone(tel, "전화번호 형식이 잘못되었습니다.");
+
+            regexHelper.isValue(postcode, "우편번호를 검색해주세요.");
+            regexHelper.isNum(postcode, "우편번호는 숫자로만 입력해주세요.");
+            regexHelper.isValue(addr1, "주소를 검색해주세요.");
+            regexHelper.isValue(addr2, "상세주소를 입력해주세요.");
+
+            regexHelper.isMatch(agreeServiceCheck, "Y", "이용약관 동의는 필수 항목입니다.");
+            regexHelper.isMatch(agreePrivacyCheck, "Y", "개인정보처리방침 약관 동의는 필수 항목입니다.");
         } catch (StringFormatException e) {
             return restHelper.badRequest(e);
         }
 
+        /** 아이디 중복 검사 */
+        try {
+            memberService.isUniqueUserId(userId);
+        } catch (Exception e) {
+            return restHelper.badRequest(e);
+        }
+
+        /** 전달할 객체 구성 */
         Member input = new Member();
         input.setUserId(userId);
         input.setUserPw(userPw);
@@ -76,6 +117,7 @@ public class MemberRestController {
         input.setIsSmsAgree(isSmsAgree);
         input.setIsEmailAgree(isEmailAgree);
 
+        /** 전송 */
         try {
             memberService.addItem(input);
         } catch (Exception e) {
