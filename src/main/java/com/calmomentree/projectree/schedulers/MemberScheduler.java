151a603ba130a78402229ebaed22aa2dfc660f2b
ingyeom <kig7174@naver.com>
2024-11-26 21:00:34 +0900

    Feat: 24/11/26 Scheduler 생성
    회원 삭제 처리를 위한 스케쥴러 생성
    
    Related to #45

---
+++ src/main/java/com/calmomentree/projectree/schedulers/MemberScheduler.java
@@ -0,0 +1,13 @@
+package com.calmomentree.projectree.schedulers;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@EnableAsync
+public class MemberScheduler {
+
+}
