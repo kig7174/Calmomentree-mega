9d5da3f2105480906fce6cd469735585418c54d9
bongja <jiuk0209@gmail.com>
2024-11-27 19:18:35 +0900

    Feat : 24/11/27 basket ,  orderItem > service, impl 추가. #55

---
+++ src/main/java/com/calmomentree/projectree/services/BasketService.java
@@ -0,0 +1,19 @@
+package com.calmomentree.projectree.services;
+
+import java.util.List;
+
+import com.calmomentree.projectree.models.Basket;
+
+public interface BasketService {
+    public Basket addItem(Basket params) throws Exception;
+
+    public Basket editItem(Basket params) throws Exception;
+
+    public int deleteItem(Basket params) throws Exception;
+
+    public Basket getItem(Basket params) throws Exception;
+
+    public List<Basket> getList(Basket params) throws Exception;
+
+    public int getCount(Basket params) throws Exception;
+}
