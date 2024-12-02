e9a210c0176163a29a06f07ff14a9dc32562ef07
garibong <jiuk0209@gmail.com>
2024-12-02 23:45:03 +0900

    Comment : 24/12/02 주문페이지 model 주석추가 #56

--- src/main/java/com/calmomentree/projectree/models/Order.java
+++ src/main/java/com/calmomentree/projectree/models/Order.java
@@ -6,23 +6,27 @@ import lombok.Setter;
 
 @Data
 public class Order {
-    private int orderId;        // 주문 일련번호
-    private String orderNo;     // 주문 번호
-   private String orderState;   // 주문 상태
-   private String orderDate;    // 주문 날짜
-   private String memberName;   // 주문자 이름
-   private String memberEmail;  // 주문자 이메일
-   private int memberPostcode;  // 주문자 우편번호
-   private String memberAddr1;  // 주문자 기본주소
-   private String memberAddr2;  // 주문자 상세주소
-   private String memberTel;    // 주문자 전화번호
-   private String receiverName; // 받는 사람 이름
-   private int receiverPostcode; // 받는 사람 우편번호
-    
-    
+    private int orderId;            // 주문 일련번호
+    private String orderNo;         // 주문 번호
+    private String orderState;      // 주문 상태
+    private String orderDate;       // 주문 날짜
+    private String memberName;      // 주문자 이름
+    private String memberEmail;     // 주문자 이메일
+    private int memberPostcode;     // 주문자 우편번호
+    private String memberAddr1;     // 주문자 기본주소
+    private String memberAddr2;     // 주문자 상세주소
+    private String memberTel;       // 주문자 전화번호
+    private String receiverName;    // 받는 사람 이름
+    private int receiverPostcode;   // 받는 사람 우편번호
+    private String receiverAddr1;   // 받는 사람 기본주소
+    private String receiverAddr2;   // 받는 사람 상세주소
+    private String receiverTel;     // 받는 사람 전화번호
+    private int totalPrice;         // 총 결제 가격
+    private String req;             // 배송요청사항
+    private int memberId;           // 결제한 회원번호
 
-    private String regDate;
-    private String editDate;
+    private String regDate;         // 등록일시
+    private String editDate;        // 변경일시
 
     @Getter
     @Setter
