338b7acb44473ffdd7d019d4e5a4245d956e070e
ingyeom <kig7174@naver.com>
2024-12-04 18:03:21 +0900

    Feat: 24/12/04 상품 이미지 및 가격 텍스트 수정
    마우스 오버 이미지 추가 / 상품 할인에 따라 달라지는 텍스트 코드 추가
    
    Related to #53

--- src/main/resources/static/assets/js/list.js
+++ src/main/resources/static/assets/js/list.js
@@ -4,20 +4,20 @@
 
 
 // image mouse over
-// const mouseover = document.querySelectorAll('.list-item-img');
-// const visible = document.querySelectorAll('.list-img-visibility');
-// const hidden = document.querySelectorAll('.list-img-hidden');
+const mouseover = document.querySelectorAll('.item-img');
+const visible = document.querySelectorAll('.list-item-img');
+const hidden = document.querySelectorAll('.hover-item-img');
 
-// for (let i = 0; i < mouseover.length; i++) {
-//     if (hidden[i].classList.contains("list-img-hidden")) {
-//         mouseover[i].addEventListener('mouseover', e => {
-//             visible[i].style.display = "none";
-//             hidden[i].style.display = "block"
-//         });
+for (let i = 0; i < mouseover.length; i++) {
+    if (hidden[i].classList.contains("hover-item-img")) {
+        mouseover[i].addEventListener('mouseover', e => {
+            visible[i].style.display = "none";
+            hidden[i].style.display = "block"
+        });
 
-//         mouseover[i].addEventListener('mouseout', e => {
-//             visible[i].style.display = "block";
-//             hidden[i].style.display = "none"
-//         });
-//     }
-// }
+        mouseover[i].addEventListener('mouseout', e => {
+            visible[i].style.display = "block";
+            hidden[i].style.display = "none"
+        });
+    }
+}
