060f70da36646ce4f9f5ae7b66524a57143f6a65
ingyeom <kig7174@naver.com>
2024-12-04 18:02:31 +0900

    Feat: 24/12/04 팝업 박스 추가
    JS로 쿠키 설정, 체크박스 선택 후 닫을 시 쿠키 기간 1일
    
    Related to #53

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -1,3 +1,16 @@
+// popup cookie
+if (!utilHelper.getCookie("no-popup")) {
+    document.querySelector(".popup").style.display = "block";
+}
+
+document.querySelector("#close-popup").addEventListener("click", (e) => {
+    document.querySelector(".popup").style.display = "none";
+
+    if (document.querySelector("#no-open").checked) {
+        utilHelper.setCookie("no-popup", "Y", 60*60*24);
+    }
+});
+
 // best seller swiper
 let bestSeller = new Swiper(".best-seller", {
     slidesPerView: "auto",
