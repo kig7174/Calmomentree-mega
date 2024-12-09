fb97eb08725b735ca4f9b1f7d31f8eb09d763047
bongja <jiuk0209@gmail.com>
2024-12-09 17:38:30 +0900

    Chore : 24/12/09 404페이지 PageOpenController 경로 추가

--- src/main/java/com/calmomentree/projectree/controllers/PageOpenController.java
+++ src/main/java/com/calmomentree/projectree/controllers/PageOpenController.java
@@ -2,8 +2,6 @@ package com.calmomentree.projectree.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class PageOpenController {
@@ -152,5 +150,11 @@ public class PageOpenController {
     public String amenityRead() {
         return "board/amenity/read";
     }
+
+    @GetMapping("/404")
+    public String errorPage() {
+        return "404";
+    }
+    
     
 }
