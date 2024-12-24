72dbc33c00e9ab204a05701cc5b98423150e5f74
ingyeom <kig7174@naver.com>
2024-12-24 17:36:46 +0900

    Feat: 24/12/24 매출 API 추가

--- src/main/java/com/calmomentree/projectree/controllers/apis/SaleRestController.java
+++ src/main/java/com/calmomentree/projectree/controllers/apis/SaleRestController.java
@@ -1,16 +1,38 @@
 package com.calmomentree.projectree.controllers.apis;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.calmomentree.projectree.helpers.RestHelper;
+import com.calmomentree.projectree.models.Sale;
+import com.calmomentree.projectree.services.SaleService;
+
 @RestController
 public class SaleRestController {
-    
-    @GetMapping("/api/sales")
-    public Map<String, Object> sales() {
+    @Autowired
+    private RestHelper restHelper;
 
-        return null;
+    @Autowired
+    private SaleService saleService;
+
+    @GetMapping("/api/sales")
+    public Map<String, Object> salesMonth() {
+        List<Sale> month = null;
+
+        try {
+            month = saleService.getList(null);
+        } catch (Exception e) {
+            return restHelper.serverError(e);
+        }
+
+        Map<String, Object> data = new LinkedHashMap<String, Object>();
+        data.put("item", month);
+
+        return restHelper.sendJson(data);
     }
 }
