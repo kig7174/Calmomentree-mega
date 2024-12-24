9a3abac28bdda07223980fab5798585815295ded
ingyeom <kig7174@naver.com>
2024-12-24 14:23:18 +0900

    Feat: 24/12/24 API Swagger 추가

---
+++ src/main/java/com/calmomentree/projectree/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.calmomentree.projectree;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+}
