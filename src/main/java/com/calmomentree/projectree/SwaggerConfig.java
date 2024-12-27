7e1eb1527d3b78a78ae0d141058d43a95e8fb394
garibong <jiuk0209@gmail.com>
2024-12-27 10:57:07 +0900

    Swagger Test

--- src/main/java/com/calmomentree/projectree/SwaggerConfig.java
+++ src/main/java/com/calmomentree/projectree/SwaggerConfig.java
@@ -3,7 +3,21 @@ package com.calmomentree.projectree;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SwaggerConfig {
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
+
+
+@Configuration      // 스프링 실행시 설정파일을 읽기위한 어노테이션
+public class SwaggerConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().components(new Components()).info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info().title("Myshop Swagger").description("Myshop REST API").version("1.0.0");
+    }
 }
